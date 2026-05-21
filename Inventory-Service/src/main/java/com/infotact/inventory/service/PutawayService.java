2ad3299d0f7b43aaa86a623b46fd26f03cddc417
Sumedho3 <sumedhhirekhan9@gmail.com>
2026-05-21 23:17:53 +0530

    corrected putawayservice logic

---
+++ Inventory-Service/src/main/java/com/infotact/inventory/service/PutawayService.java
@@ -0,0 +1,9 @@
+package com.infotact.inventory.service;
+
+import com.infotact.inventory.entity.Product;
+import com.infotact.inventory.entity.StorageBin;
+
+public interface PutawayService {
+	
+	public StorageBin findAvailableBin(Long warehouseId, Integer incomingQuantity, Product product);
+}
